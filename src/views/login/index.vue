530b1b7f6bf97c796e4f7d5ff82e930756d33570
李长伟 <568084968@qq.com>
2020-05-09 23:34:01 +0800

    登录页面布局及样式调整

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="login-container">登录页面</div>
+  <div class="login-container">
+    <!-- 导航栏 -->
+    <van-nav-bar
+      class="app-navbar"
+      title="注册 / 登录"
+      left-arrow
+      @click-left="onClickLeft"
+    />
+    <!-- /导航栏 -->
+
+    <!-- 登录表单 -->
+    <van-cell-group>
+      <van-field
+        v-model="user.mobile"
+        left-icon="smile-o"
+        placeholder="请输入手机号"
+      />
+      <van-field
+        v-model="user.code"
+        clearable
+        left-icon="music-o"
+        placeholder="请输入验证码"
+      >
+        <template #button>
+          <van-button size="small">发送验证码</van-button>
+        </template>
+      </van-field>
+    </van-cell-group>
+    <!-- /登录表单 -->
+
+    <!-- 登录按钮 -->
+    <div class="login-btn-wrap">
+      <van-button class="login-btn" type="info">登录</van-button>
+    </div>
+    <!-- /登录按钮 -->
+  </div>
 </template>
 
 <script>
@@ -8,14 +43,31 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    onClickLeft () {}
+  }
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+  .login-btn-wrap {
+    padding: 20px;
+    font-size: 15px;
+    .login-btn {
+      width: 100%;
+      background: #333;
+      border: 0;
+    }
+  }
+</style>
